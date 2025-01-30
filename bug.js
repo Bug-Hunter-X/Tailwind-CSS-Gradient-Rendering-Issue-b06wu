8184f572da9c378edfd2d8950f26cbd917ac6c47
BugHunter-X <askzenai@gmail.com>
2025-01-30 07:08:20 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4 rounded-lg shadow-md">
+  <p>This is some text.</p>
+</div>
+```
+This code uses Tailwind's gradient utility classes, but sometimes gradients might not render correctly, especially in older browsers or when there are conflicting CSS rules.  The issue could manifest as the gradient not showing at all, only showing one color, or showing unexpected banding.
